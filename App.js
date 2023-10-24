f108a2c7f01cf6242616dd9d8caeeabfd0c55194
kaiser <kaiserstyve2@gmail.com>
2023-10-24 18:57:39 +0200

    New changes

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import 'react-native-url-polyfill/auto'
 import { StatusBar } from 'expo-status-bar'
 import { useEffect, useState } from 'react'
-import { Text, View, Image, ActivityIndicator } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { BottomSheetModalProvider, } from '@gorhom/bottom-sheet'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
-import AsyncStorage from '@react-native-async-storage/async-storage'
 
 // Import navigators
 const Drawer = createDrawerNavigator()
@@ -17,12 +15,12 @@ const Stack = createNativeStackNavigator()
 // Import components
 import BottomTabNavigator from './src/components/BottomTabNavigator'
 import CustomDrawer from './src/components/CustomDrawer'
-import OnBoarding from './src/components/OnBoarding'
 
 // Import screens
 import ProfileScreen from './src/screens/ProfileScreen'
 import EditProfileScreen from './src/screens/EditProfileScreen'
 import AuthScreen from './src/screens/AuthScreen'
+import OnBoarding from './src/components/OnBoarding'
 import PropertyScreen from './src/screens/PropertyScreen'
 import AddPropertyScreen from './src/screens/AddPropertyScreen'
 import CreateAlertScreen from './src/screens/CreateAlertScreen'
@@ -32,11 +30,8 @@ import homeBlue from './assets/navicons/home-blue.png'
 
 import { auth } from './firebase'
 
-const Loading = () => {
-	<View>
-		<ActivityIndicator size={large} />
-	</View>
-}
+import messaging from '@react-native-firebase/messaging'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const DrawerNavigation = () => {
 	return (
@@ -60,99 +55,151 @@ const DrawerNavigation = () => {
 }
 
 export default function App() {
-	const [isLoading, setIsLoading] = useState(false)
-	const [viewedOnboarding, setViewedOnboarding] = useState(false)
 	const [isLoggedIn, setIsLoggedIn] = useState(false)
+	const [isFirstOpening, setIsFirstOpening] = useState(false)
 
-	const checkOnboarding = async () => {
-		try {
-			const value = await AsyncStorage.getItem('@viewedOnboarding')
-
-			if(value !== null) {
-				setViewedOnboarding(true)
+	useEffect(() => {
+		const checkFirstOpening = async () => {
+			try {
+				const value = await AsyncStorage.getItem('firstOpening')
+				if (value !== 'true') {
+					setIsFirstOpening(true)
+				}
+			} catch (e) {
+				console.log(e)
 			}
-		} catch (error) {
-			console.log('Err @checkOnboarding: ', error)
-		} finally {
-			setViewedOnboarding(false)
+		}
+
+		const unsubscribe = auth.onAuthStateChanged(user => {
+			if (user) {
+				setIsLoggedIn(true)
+			}
+		})
+
+		checkFirstOpening()
+		return unsubscribe;
+	}, [])
+
+	const requestUserPermission = async () => {
+		const authStatus = await messaging().requestPermission();
+		const enabled =
+		  authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+		  authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+	  
+		if (enabled) {
+		  console.log('Authorization status:', authStatus);
 		}
 	}
 
-	useEffect(() => {
-		checkOnboarding()
+	// useEffect(() => {
+	// 	if(requestUserPermission()) {
+	// 		messaging().getToken().then(token => {
+	// 			console.log(token)
+	// 		})
+	// 	}
+	// 	else {
+	// 		console.log("Permission denied")
+	// 	}
 
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            if(user) {
-                setIsLoggedIn(true)
-            }
-        })
 
-		return unsubscribe
-    }, [])
+	// 	messaging()
+	// 		.getInitialNotification()
+	// 		.then(async (remoteMessage) => {
+	// 			if (remoteMessage) {
+	// 				console.log(
+	// 					'Notification caused app to open from quit state:',
+	// 					remoteMessage.notification,
+	// 				);
+	// 			}
+	// 		});
+
+	// 	messaging().onNotificationOpenedApp(async (remoteMessage) => {
+	// 		console.log(
+	// 			'Notification caused app to open from background state:',
+	// 			remoteMessage.notification,
+	// 		);
+	// 	});
+
+	// 	messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+	// 		console.log('Message handled in the background!', remoteMessage);
+	// 	});
+
+
+	// 	const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+	// 		Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+	// 	});
+
+	// 	return unsubscribe;
+	// }, [])
 	
 	return (
 		<>
-		{isLoading ? <Loading /> : viewedOnboarding ? <AuthScreen /> : <OnBoarding /> }
-			{/* {
-				!isLoggedIn 
+			{
+				isFirstOpening
+				? (
+					<OnBoarding />
+				)
+				: (
+					!isLoggedIn 
 				
-				? <AuthScreen />
-				
-				: <GestureHandlerRootView style={{ flex: 1 }}>
-					<NavigationContainer>
-						<BottomSheetModalProvider>
-							<Stack.Navigator>
-								<Stack.Group>
-									<Stack.Screen
-										name="Drawer"
-										component={DrawerNavigation}
-										options={{ 
-											headerShown: false
-										}}
-									/>
-								</Stack.Group>
-								<Stack.Group>
-									<Stack.Screen
-										name="Profile"
-										component={ProfileScreen}
-										options={{ 
-											headerShown: false
-										}}
-									/>
-									<Stack.Screen
-										name="Property"
-										component={PropertyScreen}
-										options={{ 
-											headerShown: false
-										}}
-									/>
-									<Stack.Screen
-										name="AddProperty"
-										component={AddPropertyScreen}
-										options={{ 
-											headerShown: false
-										}}
-									/>
-									<Stack.Screen
-										name="EditProfile"
-										component={EditProfileScreen}
-										options={{ 
-											headerShown: false
-										}}
-									/>
-									<Stack.Screen
-										name="CreateAlert"
-										component={CreateAlertScreen}
-										options={{ 
-											headerShown: false
-										}}
-									/>
-								</Stack.Group>
-							</Stack.Navigator>
-						</BottomSheetModalProvider>
-					</NavigationContainer>
-				</GestureHandlerRootView>
-			} */}
+					? <AuthScreen />
+					
+					: <GestureHandlerRootView style={{ flex: 1 }}>
+						<NavigationContainer>
+							<BottomSheetModalProvider>
+								<Stack.Navigator>
+									<Stack.Group>
+										<Stack.Screen
+											name="Drawer"
+											component={DrawerNavigation}
+											options={{ 
+												headerShown: false
+											}}
+										/>
+									</Stack.Group>
+									<Stack.Group>
+										<Stack.Screen
+											name="Profile"
+											component={ProfileScreen}
+											options={{ 
+												headerShown: false
+											}}
+										/>
+										<Stack.Screen
+											name="Property"
+											component={PropertyScreen}
+											options={{ 
+												headerShown: false
+											}}
+										/>
+										<Stack.Screen
+											name="AddProperty"
+											component={AddPropertyScreen}
+											options={{ 
+												headerShown: false
+											}}
+										/>
+										<Stack.Screen
+											name="EditProfile"
+											component={EditProfileScreen}
+											options={{ 
+												headerShown: false
+											}}
+										/>
+										<Stack.Screen
+											name="CreateAlert"
+											component={CreateAlertScreen}
+											options={{ 
+												headerShown: false
+											}}
+										/>
+									</Stack.Group>
+								</Stack.Navigator>
+							</BottomSheetModalProvider>
+						</NavigationContainer>
+					</GestureHandlerRootView>	
+				)
+			}
 		</>
 	)
 }
--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,14 +1,52 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Image, Animated, Easing, SafeAreaView } from 'react-native'
 import { auth, googleProvider } from '../../../firebase'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
 
 import googleLogo from '../../../assets/logos/logo-google.png'
 import facebookLogo from '../../../assets/logos/logo-facebook.png'
 
+import Modal from "react-native-modal"
+
 const LoginForm = () => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [isModalVisible, setIsModalVisible] = useState(false)
+    const [modalContent, setModalContent] = useState({
+        title: "",
+        paragraph: ""
+    })
+    
     const email = "kaiserstyve2@gmail.com"
     const password = "123456"
+
+    const pulseAnimation = new Animated.Value(1)
+
+    const startPulseAnimation = () => {
+        pulseAnimation.setValue(1);
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(pulseAnimation, {
+                    toValue: 0.6,
+                    duration: 500,
+                    useNativeDriver: true,
+                    easing: Easing.inOut(Easing.ease),
+                }),
+                Animated.timing(pulseAnimation, {
+                    toValue: 1,
+                    duration: 500,
+                    useNativeDriver: true,
+                    easing: Easing.inOut(Easing.ease),
+                }),
+            ])
+        ).start();
+    };
+
+    useEffect(() => {
+        if (isLoading) {
+            startPulseAnimation()
+        }
+        startPulseAnimation()
+    }, [isLoading]);
 
     const handleSignUp = () => {
         createUserWithEmailAndPassword(auth, email, password)
@@ -20,15 +58,39 @@ const LoginForm = () => {
     }
 
     const handleSignIn = () => {
+        if(isLoading) {
+            return 
+        }
+        setIsLoading(true)
         signInWithEmailAndPassword(auth, email, password)
-        .then(useCredentials => {
-            const user = useCredentials.user
-            console.log("logged in with: ", user.email)
-        })
-        .catch(error => alert(error.message))
+            .then(useCredentials => {
+                const user = useCredentials.user
+                console.log("logged in with: ", user.email)
+            })
+            .catch(error => {
+                const errorCode = error.code
+                console.log(errorCode)
+                if(['auth/invalid-email', 'auth/user-not-found', 'auth/wrong-password'].includes(errorCode)) {
+                    setModalContent({ ...modalContent, title: 'Identifiants incorrects!' })
+                    // alert('Identifiants incorrects!')
+                }
+                else if(['auth/too-many-requests'].includes(errorCode)) {
+                    setModalContent({ ...modalContent, title: 'Trop de requetes!' })
+                    // alert('Trop de requetes!')
+                }
+                else {
+                    setModalContent({ ...modalContent, title: "Oops, une erreur s'est produite" })
+                    // alert("Oops, une erreur s'est produite")
+                }
+                setIsLoading(false)
+                setIsModalVisible(true)
+            })
     }
 
     const handleGoogleSignIn = () => {
+        if(isLoading) {
+            return 
+        }
         signInWithPopup(auth, googleProvider)
         //     .then((result) => {
         //         // This gives you a Google Access Token. You can use it to access the Google API.
@@ -51,7 +113,20 @@ const LoginForm = () => {
     }
 
     return (
-        <View className="flex-1">
+        <SafeAreaView className="flex-1">
+            <Modal isVisible={isModalVisible} className="flex-1">
+                <View className="flex-1 items-center justify-center">
+                    <View className="items-center justify-center w-full h-80 rounded-2xl bg-white">
+                        <Text>{modalContent?.title}</Text>
+                        <TouchableOpacity
+                            className="bg-blue-500 mt-12 rounded-xl py-4 flex-row justify-center items-center"
+                            onPress={() => setIsModalVisible(!isModalVisible)}
+                        >
+                            <Text className="text-center text-base">Close modal</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
             <View className="mt-4">
                 <Text className="text-base mb-2 ml-1">Adresse Email</Text>
                 <View className="border border-gray-200 bg-gray-50 p-4 rounded-xl">
@@ -78,26 +153,37 @@ const LoginForm = () => {
             <View className="mt-4">
                 <TouchableOpacity 
                     onPress={handleSignIn}
-                    className="rounded-xl bg-blue-500 py-4"
+                    className={`rounded-xl py-4 flex-row justify-center items-center space-x-1 ${isLoading ? 'bg-blue-500/75' : 'bg-blue-500'}`}
                 >
                     <Text className="text-base text-white text-center font-medium">Se connecter</Text>
+                    <View className={!isLoading ? "hidden" : "inline-flex relative"}>
+                        <Animated.View
+                            className="w-6 h-6 bg-white opacity-50 rounded-full"
+                            style={{
+                                transform: [{ scale: pulseAnimation }],
+                            }}
+                        />
+                        <View className="absolute top-0 w-6 h-6 items-center justify-center">
+                            <View className="w-3 h-3 bg-white opacity-80 rounded-full" />
+                        </View>
+                    </View>
                 </TouchableOpacity>
             </View>
             <Text className="text-center my-6 font-medium uppercase text-xs">Ou continuer avec</Text>
             <View className="">
                 <TouchableOpacity onPress={handleGoogleSignIn} className="rounded-xl border border-blue-500 py-3 flex-row space-x-2 justify-center items-center">
-                    <Image className="h-8 w-8 rounded-full" source={googleLogo} />
+                    <Image className="h-7 w-7 rounded-full" source={googleLogo} />
                     <Text className="text-base text-center font-medium">Google</Text>
                 </TouchableOpacity>
             </View>
             <View className="mt-4">
                 <TouchableOpacity className="rounded-xl border border-blue-500 py-3 flex-row space-x-2 justify-center items-center">
-                    <Image className="h-10 w-10 rounded-full" source={facebookLogo} />
+                    <Image className="h-8 w-8 rounded-full" source={facebookLogo} />
                     <Text className="text-base text-center font-medium">Facebook</Text>
                 </TouchableOpacity>
             </View>
             <Text className="text-center mt-8 font-medium uppercase">Ou continuer sans se connecter</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
