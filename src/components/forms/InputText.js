cad357d1db8b2b8859e35004afe4182b7a62f74a
kaiser-styve <148588319+kaiser-styve@users.noreply.github.com>
2023-10-26 05:08:58 +0000

    New changes

--- src/components/forms/InputText.js
+++ src/components/forms/InputText.js
@@ -1,10 +1,17 @@
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 
-const InputText = ({ placeholder, name, keyboardType, multiline, numberOfLines }) => {
+const InputText = ({ placeholder, name, keyboardType, multiline, numberOfLines, setFieldsValues }) => {
     const [value, setValue] = useState('')
     const [focusClasses, setFocusClasses] = useState('')
 
+    const onChangeText = (text) => {
+        setValue(text)
+        setFieldsValues((prevFieldsValues) => ({
+            ...prevFieldsValues,
+            [name]: value,
+        }))
+    }
     return (
         <View className="pb-4">
             <TextInput
@@ -17,7 +24,7 @@ const InputText = ({ placeholder, name, keyboardType, multiline, numberOfLines }
                 // maxLength={10}
                 onFocus={() => {setFocusClasses('border-blue-500')}}
                 onBlur={() => {setFocusClasses('')}}
-                onChangeText={(text) => {setValue(text)}}
+                onChangeText={onChangeText}
                 className={("text-base border border-gray-200 bg-gray-50 p-4 flex-1 rounded-xl ") + focusClasses}
             />
         </View>
